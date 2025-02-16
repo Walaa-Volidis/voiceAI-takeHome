6c22af39398ce76280a3161fa6a525726d02718a
Walaa VOILIDAS <walaavolidis@gmail.com>
2025-02-16 15:45:00 +0200

    foo

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -1,5 +1,4 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { uploadToS3 } from '../../../lib/uploadToS3';
 import { parsePdfFromBuffer } from '../../../lib/bufferToPdf';
 export async function POST(request: NextRequest) {
   try {
@@ -20,28 +19,15 @@ export async function POST(request: NextRequest) {
     const arrayBuffer = await file.arrayBuffer();
     const buffer = Buffer.from(arrayBuffer);
 
-    const extractedText = '';
     try {
       const extractedText = await parsePdfFromBuffer(buffer);
       console.log('Extracted text:', extractedText);
+      return NextResponse.json({ text: extractedText });
     } catch (error) {
       console.error('Error parsing PDF:', error);
       return NextResponse.json(
         { error: 'Failed to extract text from PDF' },
         { status: 500 }
-      );
-    }
-
-    try {
-      const publicUrl = await uploadToS3(buffer);
-      console.log('Public URL:', publicUrl);
-
-      return NextResponse.json({ url: publicUrl, text: extractedText });
-    } catch (error) {
-      console.error('Error uploading to S3:', error);
-      return NextResponse.json(
-        { error: 'Failed to upload PDF to S3' },
-        { status: 400 }
       );
     }
   } catch (error) {
